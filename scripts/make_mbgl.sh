e91d9a5d287e67fe922b73432c4e06a3a660fba3
Mike Morris <michael.patrick.morris@gmail.com>
2014-08-21 17:42:30 -0400

    more debugging

--- scripts/make_mbgl.sh
+++ scripts/make_mbgl.sh
@@ -6,3 +6,5 @@ set -o pipefail
 cd vendor/mapbox-gl-native
 make setup
 CXX11=true CFLAGS="-fPIC" CXXFLAGS="-fPIC" make mbgl
+
+cat mapnik-packaging/osx/out/build-cpp11-libstdcpp-gcc-x86_64-linux/lib/pkgconfig/glfw3.pc
