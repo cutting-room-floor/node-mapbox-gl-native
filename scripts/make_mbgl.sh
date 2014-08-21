ea5d379254a301ee822bbf2fc0fa3d7df8c9c6d9
Mike Morris <michael.patrick.morris@gmail.com>
2014-08-21 15:17:30 -0400

    fix libboost_regex path, add a littel debugging

--- scripts/make_mbgl.sh
+++ scripts/make_mbgl.sh
@@ -6,3 +6,7 @@ set -o pipefail
 cd vendor/mapbox-gl-native
 make setup
 CXX11=true CFLAGS="-fPIC" CXXFLAGS="-fPIC" make mbgl
+
+ls -la mapnik-packaging/osx/out
+ls -la mapnik-packaging/osx/out/build-cpp11-libstdcpp-gcc-x86_64-linux/lib
+ls -la mapnik-packaging/osx/out/build-cpp11-libcpp-universal/lib
